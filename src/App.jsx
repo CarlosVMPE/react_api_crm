4d3e2e2ed1845e7e02aa55aebe12ed0c9f7fe937
Carlos Vilchez Malasquez <carlosvilchez@bcp.com.pe>
2022-06-16 11:28:29 -0500

    upload env files

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Layout from "./layout/Layout";
 import Inicio from "./paginas/Inicio";
@@ -8,11 +8,10 @@ import EditarCliente from "./paginas/EditarCliente";
 import VerCliente from "./paginas/VerCliente";
 
 function App() {
-
-  console.log(import.meta.env);
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/clientes" />} />
         <Route path="/clientes" element={<Layout />}>
           <Route index element={<Inicio />} />
           <Route path="nuevo" element={<NuevoCliente />} />
